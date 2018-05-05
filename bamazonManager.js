06506d75e96e4d5d0f977b8d65cba49ebc4cd1e4
Madinah2491 <madinah2491@gmail.com>
2018-05-05 10:28:32 -0400

    fixed broken functions from last push commented further changes

--- bamazonManager.js
+++ bamazonManager.js
@@ -32,47 +32,69 @@ var viewInventory = function(){
     })
 }
 
-var addInventory = function(){
-    connection.query("INSERT INTO products (product_name, stock_quantity) VALUES ('answers.addInventory', 'answers.addStockQuantity')", function(err, result, fields){
-        connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    })
-}
 
 function manager(){
     inquirer.prompt({
-        name:"saleItems",
+        name:"saleItems",//: displayProducts ,
         type:"rawlist",
         message:"View items for sale",
-        choices:["yes", "no, view low inventory", "other"]
+        choices:["yes", "no, view low inventory"]
     }).then(function(answer){
         if (answer.saleItems=="yes"){
             viewProducts();
         }else { 
             viewInventory();
-        }else {
-            inventory();
         }
     })
-});    
-
-function inventory(){
-    inquirer.prompt({
-        name:"moreInventory",
-        type:"multi",
-        message:"Would you like to restock inventory?",
-        choices:["yes", "no, add new product"]
-    }).then(function(answer){
-        if (answer.moreInventory == "yes"){
-            addInventory();
-        }else {
-            new
-        }
-    })
-}
+};    
 manager();    
 
 // * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
 
 // * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+
+
+// var addInventory = function(){
+//     connection.query("INSERT INTO products (product_name, stock_quantity) VALUES ('answers.addInventory', 'answers.addStockQuantity')", function(err, result, fields){
+//         connection.query(sql, function (err, result) {
+//         if (err) throw err;
+//         console.log(result);
+//     })
+// }
+
+// function manager(){
+//     inquirer.prompt({
+//         name:"saleItems",
+//         type:"rawlist",
+//         message:"View items for sale",
+//         choices:["yes", "no, view low inventory", "other"]
+//     }).then(function(answer){
+//         if (answer.saleItems=="yes"){
+//             viewProducts();
+//         }else { 
+//             viewInventory();}
+//         // }else {
+//         //     inventory();
+//         // }
+//     })
+// });    
+
+// // function inventory(){
+// //     inquirer.prompt({
+// //         name:"moreInventory",
+// //         type:"multi",
+// //         message:"Would you like to restock inventory?",
+// //         choices:["yes", "no, add new product"]
+// //     }).then(function(answer){
+// //         if (answer.moreInventory == "yes"){
+// //             addInventory();
+// //         }else {
+// //             new
+// //         }
+// //     })
+// // }
+// manager();    
+
+// // * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+
+// // * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
